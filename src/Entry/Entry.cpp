8e9b62eaec93d3edeb6300954ef73fd5d5409069
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-03-15 13:58:08 +0800

    chore: rename

--- src/Entry/Entry.cpp
+++ src/Entry/Entry.cpp
@@ -1,20 +1,20 @@
-#include "RenameThis.h"
+#include "entry.h"
 
 #include <ll/api/plugin/NativePlugin.h>
 #include <memory>
 
-namespace rename_this {
+namespace entry {
 
-RenameThis::RenameThis() = default;
+entry::entry() = default;
 
-RenameThis& RenameThis::getInstance() {
-    static RenameThis instance;
+entry& entry::getInstance() {
+    static entry instance;
     return instance;
 }
 
-ll::plugin::NativePlugin& RenameThis::getSelf() const { return *mSelf; }
+ll::plugin::NativePlugin& entry::getSelf() const { return *mSelf; }
 
-bool RenameThis::load(ll::plugin::NativePlugin& self) {
+bool entry::load(ll::plugin::NativePlugin& self) {
     mSelf = std::addressof(self);
     getSelf().getLogger().info("loading...");
 
@@ -23,7 +23,7 @@ bool RenameThis::load(ll::plugin::NativePlugin& self) {
     return true;
 }
 
-bool RenameThis::enable() {
+bool entry::enable() {
     getSelf().getLogger().info("enabling...");
 
     // Code for enabling the plugin goes here.
@@ -31,7 +31,7 @@ bool RenameThis::enable() {
     return true;
 }
 
-bool RenameThis::disable() {
+bool entry::disable() {
     getSelf().getLogger().info("disabling...");
 
     // Code for disabling the plugin goes here.
@@ -41,18 +41,18 @@ bool RenameThis::disable() {
 
 extern "C" {
 _declspec(dllexport) bool ll_plugin_load(ll::plugin::NativePlugin& self) {
-    return RenameThis::getInstance().load(self);
+    return entry::getInstance().load(self);
 }
 
-_declspec(dllexport) bool ll_plugin_enable(ll::plugin::NativePlugin&) { return RenameThis::getInstance().enable(); }
+_declspec(dllexport) bool ll_plugin_enable(ll::plugin::NativePlugin&) { return entry::getInstance().enable(); }
 
-_declspec(dllexport) bool ll_plugin_disable(ll::plugin::NativePlugin&) { return RenameThis::getInstance().disable(); }
+_declspec(dllexport) bool ll_plugin_disable(ll::plugin::NativePlugin&) { return entry::getInstance().disable(); }
 
 /// @warning Unloading the plugin may cause a crash if the plugin has not released all of its
 /// resources. If you are unsure, keep this function commented out.
 // _declspec(dllexport) bool ll_plugin_unload(ll::plugin::NativePlugin&) {
-//     return RenameThis::getInstance().unload();
+//     return entry::getInstance().unload();
 // }
 }
 
-} // namespace rename_this
+} // namespace entry
