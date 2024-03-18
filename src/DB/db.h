0cc4a1d6f09483f4a62ac59ea6311cdcd5909033
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-03-18 21:11:15 +0800

    chore: format db.h

--- src/DB/db.h
+++ src/DB/db.h
@@ -15,12 +15,12 @@ std::unique_ptr<ll::data::KeyValueDB>&       getInstance();
 
 bool loadLevelDB();
 
-json                    to_json(const perm::structs::PluginPermData& permData);
+json                          to_json(const perm::structs::PluginPermData& permData);
 perm::structs::PluginPermData from_json(const json& j);
 
 std::optional<perm::structs::PluginPermData> getPluginData(string pluginName);
-bool                                   setPluginData(string pluginName, perm::structs::PluginPermData& data);
-bool                                   initPluginData(string pluginName);
-bool                                   isPluginInit(string pluginName);
+bool                                         setPluginData(string pluginName, perm::structs::PluginPermData& data);
+bool                                         initPluginData(string pluginName);
+bool                                         isPluginInit(string pluginName);
 
 } // namespace pcore::db
